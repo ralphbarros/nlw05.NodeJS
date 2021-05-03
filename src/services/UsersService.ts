dc8adf83508121345096734e2abc4478be5ba883
Ralph Barros Marinho <ralphbarros@gmail.com>
2021-05-03 09:21:42 -0300

    Add files via upload

---
+++ src/services/UsersService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+
+class UsersService {
+    private usersRepository: Repository<User>
+    constructor(){
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string) {
+        
+        //Verificar se user existe
+        const userExists = await this.usersRepository.findOne({email});
+        //Se existir, retornar user
+
+        if(userExists){
+            return userExists;
+        }
+        //Se não existe, salvar no BD
+        const user = this.usersRepository.create({
+            email
+        });
+        await this.usersRepository.save(user);
+        return user;
+        
+    }
+
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({ email });
+    
+        return user;
+      }
+}
+
+export { UsersService };
